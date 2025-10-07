d7ceecced0a7bf88ee78316b4db09b48b125ba4d
Hiral Prajapati <hiral@example.com>
2025-10-07 16:20:56 +0530

    Update jobRoutes.js in backend

--- backend/src/routes/jobRoutes.js
+++ backend/src/routes/jobRoutes.js
@@ -5,6 +5,6 @@ import { protect, authorizeRoles } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.post("/", protect, authorizeRoles("employer", "admin"), createJob);
-router.get("/", getAllJobs);
+router.get("/job", getAllJobs);
 
 export default router;
